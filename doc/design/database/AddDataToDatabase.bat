4a764de3da115081cff2978aa79f3f625931fea5
EmVH <hoaiem.heli22@gmail.com>
2018-10-24 17:18:21 +0700

    Update Script DB design

--- doc/design/database/AddDataToDatabase.bat
+++ doc/design/database/AddDataToDatabase.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
-ECHO Adding: Create data for the database fptueventclub...
+ECHO Adding: Create data for the database fptueventclub . . .
 mysql -u fptueventclub -pFPTUEventClub@123 fptueventclub < FPTUEventClub_Script_StaticData.sql
--- doc/design/database/ResetDatabase.bat
+++ doc/design/database/ResetDatabase.bat
@@ -5,5 +5,5 @@ mysql -u root -p  < FPTUEventClub_Drop_Database.sql
 ECHO Step 2: Input the password of root to create the database.
 mysql -u root -p  < FPTUEventClub_Script_CreateDB_MySQL.sql
 
-ECHO Step 3: Create tables for the database fptueventclub...
+ECHO Step 3: Create tables for the database fptueventclub . . .
 mysql -u fptueventclub -pFPTUEventClub@123 fptueventclub < FPTUEventClub_Script_Table_MYSQL.sql
